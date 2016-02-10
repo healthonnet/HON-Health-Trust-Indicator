4715ca02a565c5b39727f53a77b47beaa3e25b31
cedric <cedric.frossard@healthonnet.org>
2016-02-10 13:15:00 +0100

    KCON-147 #comment get urls, call readability api and print results on google search

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,3 +1,20 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Event Page');
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log('previousVersion', details.previousVersion);
+});
+
+
+
+chrome.webRequest.onCompleted.addListener(function(details) {
+  chrome.tabs.executeScript(details.tabId, {
+    file: 'contentscript.js',
+    allFrames: true
+  });
+}, {
+  urls: ['http://google.*/*'],
+  types: ['xmlhttprequest']
+});
+
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,4 +2,46 @@
 
 console.log('\'Allo \'Allo! Content script');
 
+//Config
+var colors = {
+  'easy': 'limegreen',
+  'average': '#ffc520',
+  'difficult': 'crimson'
+};
 
+
+var updateLinks = function(){
+  //clean DOM
+  $('div.rc .s .hon.rdb').remove();
+
+  //Get links
+  var links = [];
+  var nodeList = document.querySelectorAll('div.rc h3.r a');
+
+  for (var i = 0; i < nodeList.length; ++i) {
+    links[i] = nodeList[i].href;
+  }
+
+  links.forEach(function(link, index){
+    //TODO Must be HTTPS (otherwise: rejected by chrome)
+
+    //Readability
+    $.get( 'http://api.kconnect.honservices.org/~kconnect/cgi-bin/readability.cgi?data={"url":"' + link + '"}', function( data ) {
+      //TODO tester la pertinence du résultat avant affichage
+      var html =
+        '<span class="hon rdb" style="color: ' + colors[data.readability.difficulty] + '; display: none;">' +
+        'Rdb : ' + data.readability.score + '(' + data.readability.difficulty + ')' +
+        '</span>';
+      $(nodeList.item(index)).parent().parent().children('.s').prepend(html);
+    });
+
+    //Trustability
+    /*$.get( 'http://api.kconnect.honservices.org/~kconnect/cgi-bin/trustability.cgi?data={"url":"'+link+'"}', function( data ) {
+     //On test la pertinence du résultat avant affichage
+     console.log(data);
+     });*/
+
+  });
+};
+
+updateLinks();
