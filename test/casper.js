8852d68ff0168c38eb2fc288909a3e818db6dbcc
cedric <cedric.frossard@healthonnet.org>
2016-02-18 10:34:34 +0100

    Yahoo integration tests

--- test/casper.js
+++ test/casper.js
@@ -11,9 +11,9 @@ casper.test.begin('Add Readability & Trustability Information in Google search r
     });
 
     //Debug
-    casper.on("remote.message", function(message) {
+    /*casper.on("remote.message", function(message) {
         this.echo("remote console.log: " + message);
-    });
+    });*/
 
     casper.then(function() {
 
@@ -40,6 +40,57 @@ casper.test.begin('Add Readability & Trustability Information in Google search r
 
         test.assertTitle("vidal - Recherche Google", "google title is ok");
         test.assertUrlMatch(/q=vidal/, "search term has been submitted");
+
+        test.assertEval(function() {
+            return __utils__.findAll(".hon.trb").length == 1;
+        }, "trustability informations found");
+        test.assertEval(function() {
+            return __utils__.findAll(".hon.rdb").length == 1;
+        }, "readability informations found");
+
+    });
+
+    casper.run(function() {
+        test.done();
+    });
+});
+// Yahoo
+casper.test.begin('Add Readability & Trustability Information in Yahoo search results', 6, function suite(test) {
+
+    casper.start("https://fr.yahoo.com/", function() {
+        test.assertExists('form[action="https://fr.search.yahoo.com/search"]', "main form is found");
+        this.fill('form[action="https://fr.search.yahoo.com/search"]', {
+            p: "vidal"
+        }, true);
+        test.assertExists('input[name="p"]', "input form is found");
+        this.wait(1000);
+    });
+
+    casper.then(function() {
+
+        //Chrome API polyfill
+        this.evaluate(function (chrome){
+            chrome.i18n.getMessage = function (message) {
+                return message;
+            };
+
+            window.chrome = chrome;
+        },chrome);
+
+        this.page.injectJs('app/bower_components/jquery/dist/jquery.min.js');
+        this.page.injectJs('app/scripts/contentscript.js');
+
+        this.evaluate(function () {
+            var trustabilityReq,
+                readabilityReq;
+            trustabilityReq = __utils__.sendAJAX('http://api.kconnect.honservices.org/~kconnect/cgi-bin/is-trustable.cgi?domain=vidal.fr', "GET");
+            readabilityReq  = __utils__.sendAJAX('http://api.kconnect.honservices.org/~kconnect/cgi-bin/readability.cgi?data={"url":"https://www.vidal.fr"}', "GET");
+            trustabilityCallback(JSON.parse(trustabilityReq), $(document.querySelectorAll('h3.title a').item(1)).parent().siblings('div:first'));
+            readabilityCallback( JSON.parse(readabilityReq), $(document.querySelectorAll('h3.title a').item(1)).parent().siblings('div:first'), "https://www.vidal.fr");
+        });
+
+        test.assertTitle("vidal - Yahoo Search - Actualités", "yahoo title is ok");
+        test.assertUrlMatch(/p=vidal/, "search term has been submitted");
 
         test.assertEval(function() {
             return __utils__.findAll(".hon.trb").length == 1;
