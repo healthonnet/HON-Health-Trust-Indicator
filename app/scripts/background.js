9e4510bdac82d209cf19fc3dcb4e8785a11d2a37
cedric <cedric.frossard@healthonnet.org>
2016-02-19 10:57:33 +0100

    optimize Firefox WebExtensions compatibility

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,5 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
-});
-
 chrome.webRequest.onCompleted.addListener(function(details) {
   chrome.tabs.executeScript(
     details.tabId, {file: 'bower_components/jquery/dist/jquery.min.js', allFrames: true}
