69ae99a41e9ca04ab8e3c3fa966ec438dc6b2554
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2016-03-03 16:45:19 +0100

    added options

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+function store(input) {
+  console.log('does something');
+  chrome.storage.sync.set({
+    results: input.value
+  });
+}
+
+// TODO: extends
+// if (localStorage.results) {
+//
+// }
+
+var inputs = document.getElementsByTagName('input');
+for (var i = 0; i < inputs.length; i++) {
+  if (inputs[i].type === 'radio') {
+    inputs[i].addEventListener('change', store(inputs[i]));
+  }
+}
