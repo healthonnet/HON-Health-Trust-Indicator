5d551fddc1b153eb69bec40006b6c896c3a49ea3
cedric <cedric.frossard@healthonnet.org>
2016-03-29 13:29:34 +0200

    test community ratings from siteJabber

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -10,10 +10,11 @@ chrome.tabs.query(query, function(tabs) {
   var trustabilityRequest = kconnect.getIsTrustable(domain);
   var readabilityRequest = kconnect.getReadability(currentTab.url);
   kconnect.displayHONcodeStatus(domain);
+  var siteJabberRequest = kconnect.getSiteJabber(domain);
 
-  $.when(trustabilityRequest, readabilityRequest)
-    .then(function(trustabilityResponse, readabilityResponse) {
-
+  $.when(trustabilityRequest, readabilityRequest, siteJabberRequest)
+    .then(function(trustabilityResponse, readabilityResponse
+      , siteJabberResponse) {
       // Trustability Informations
       if (trustabilityResponse[0].trustability === undefined) {
         $('#trustability').html('<p>No Trustability informations ' +
@@ -43,6 +44,26 @@ chrome.tabs.query(query, function(tabs) {
         '<p class="' + difficulty + '">' +
         kconnect.config.difficultyKeyword[difficulty] +
         '</p>');
+
+
+      $('#users').html(
+        '<h3>Community rating : </h3>' +
+        '<div id="stars"></div>' +
+        '<span class="credit"><a href="http://www.sitejabber.com/about-us">' +
+        'Powered by SiteJabber</a></span>'
+
+      );
+
+      var raterOptions = {
+        max_value: 5,
+        step_size: 0.1,
+        initial_value: JSON.parse(siteJabberResponse[0])
+          .averageRating[0].rating,
+        selected_symbol_type: 'utf8_star', // Must be a key from symbols
+        readonly: true,
+      };
+      $('#stars').rate(raterOptions);
+
 
       var progress = new CircularProgress({
         radius: 40,
