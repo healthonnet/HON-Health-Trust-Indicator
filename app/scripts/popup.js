50ab12b0a05cc6f998a93dfdc16112db7fc57f5b
cedric <cedric.frossard@healthonnet.org>
2016-06-06 16:58:26 +0200

    better css

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -12,9 +12,36 @@ chrome.tabs.query(query, function(tabs) {
   kconnect.displayHONcodeStatus(domain);
   var siteJabberRequest = kconnect.getSiteJabber(domain);
 
+  function siteJabberInformations(siteJabberResponse){
+    $('#users').html(
+      '<h3>Community ratings (' +
+      JSON.parse(siteJabberResponse[0])
+        .numReviews[0].rating + ') : </h3>' +
+      '<div id="stars"></div>' +
+      '<span class="credit"><a href="http://www.sitejabber.com/about-us">' +
+      'Powered by SiteJabber</a></span>'
+
+    );
+
+    var raterOptions = {
+      max_value: 5,
+      step_size: 0.1,
+      initial_value: JSON.parse(siteJabberResponse[0])
+        .averageRating[0].rating,
+      selected_symbol_type: 'utf8_star', // Must be a key from symbols
+      readonly: true,
+    };
+    $('#stars').rate(raterOptions);
+
+  }
+
   $.when(trustabilityRequest, readabilityRequest, siteJabberRequest)
     .then(function(trustabilityResponse, readabilityResponse
       , siteJabberResponse) {
+
+      // SiteJabber informations
+      siteJabberInformations(siteJabberResponse);
+
       // Trustability Informations
       if (trustabilityResponse[0].trustability === undefined) {
         $('#trustability').html('<p>No Trustability informations ' +
@@ -45,28 +72,6 @@ chrome.tabs.query(query, function(tabs) {
         kconnect.config.difficultyKeyword[difficulty] +
         '</p>');
 
-
-      $('#users').html(
-        '<h3>Community ratings (' +
-        JSON.parse(siteJabberResponse[0])
-          .numReviews[0].rating + ') : </h3>' +
-        '<div id="stars"></div>' +
-        '<span class="credit"><a href="http://www.sitejabber.com/about-us">' +
-        'Powered by SiteJabber</a></span>'
-
-      );
-
-      var raterOptions = {
-        max_value: 5,
-        step_size: 0.1,
-        initial_value: JSON.parse(siteJabberResponse[0])
-          .averageRating[0].rating,
-        selected_symbol_type: 'utf8_star', // Must be a key from symbols
-        readonly: true,
-      };
-      $('#stars').rate(raterOptions);
-
-
       var progress = new CircularProgress({
         radius: 40,
         strokeStyle: 'limegreen',
@@ -81,6 +86,7 @@ chrome.tabs.query(query, function(tabs) {
       $('#trustability').html('<p>No Trustability informations for' +
         ' this domain</p>');
       $('#readability').html('<p>No Readability  informations</p>');
+      $('#users').html('<p>No Community  informations</p>');
     });
 });
 
