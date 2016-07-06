29688b33013a3a13832b5a22bf206dcb6247ec9c
Pierre Repetto-Andipatin <pierre.repetto@gmail.com>
2016-07-06 13:09:46 +0200

    remove lang for omnibox

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -30,7 +30,7 @@ chrome.omnibox.onInputEntered.addListener(function(text) {
   var url = 'http://everyone.khresmoi.eu/hon-search';
   if (text) {
     url += '?start=0&fq=docType:html&group.field=domain' +
-      '&overrideQ=&searchLanguage=en&q=' + encodeURIComponent(text);
+      '&overrideQ=&q=' + encodeURIComponent(text);
   }
   chrome.tabs.getSelected(null, function(tab) {
     chrome.tabs.update(tab.id, {url: url});
