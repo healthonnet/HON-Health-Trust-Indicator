fd6f048827ecb93b5a730a736387bd23469c114f
cedric frossard <adrion.dev@gmail.com>
2017-10-10 08:34:31 +0200

    handle infinite scroll

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -287,6 +287,9 @@ var updateLinks = function() {
   else if (window.location.host.indexOf('duckduckgo') > -1) {
     hrefSelector = 'h2.result__title>a.result__a';
     timeToWait = 1000;
+    $('head').bind('DOMSubtreeModified', function() {
+      updateLinks();
+    });
   }
 
   setTimeout(function() {
